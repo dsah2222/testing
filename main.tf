628fc7086a0e6e04830b8cd2efe3b8ad38751379
dsah2222 <dileepsah256@gmail.com>
2024-09-18 17:56:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,3 +48,26 @@ resource "null_resource" "wait_for_task_completion" {
     EOT
   }
 }
+
+
+resource "null_resource" "run_task" {
+  provisioner "local-exec" {
+    command = <<EOT
+      aws ecs run-task --cluster example-cluster --task-definition ${aws_ecs_task_definition.example.family} --launch-type FARGATE --network-configuration "awsvpcConfiguration={subnets=[subnet-12345678],securityGroups=[sg-12345678],assignPublicIp=ENABLED}"
+    EOT
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "null_resource" "wait_for_task_completion" {
+  depends_on = [null_resource.run_task]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws ecs wait tasks-stopped --cluster example-cluster --tasks $(aws ecs list-tasks --cluster example-cluster --desired-status STOPPED --query 'taskArns[0]' --output text)
+    EOT
+  }
+}
